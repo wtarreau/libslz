015a1b2cf2c53de26b6572134a41a02b79d55274
Willy Tarreau <w@1wt.eu>
2021-05-21 19:06:42 +0200

    slz: fix broken initialization of CRC tables
    A typo was introduced in commit d8715db ("slz: do not produce the
    crc32_fast table when CRC is natively supported") causing the
    condition to initialize the tables to be inverted. As a result,
    machines which do *not* have a hardware CRC32 instruction do not
    initialize the tables. The code which was tested on haproxy did
    have it, and the tests run on ARM to validate the build didn't
    show any error.
    
    Thanks to Nicolas Boos for reporting the problem!

--- src/slz.c
+++ src/slz.c
@@ -1319,7 +1319,7 @@ void slz_prepare_dist_table()
 __attribute__((constructor))
 static void __slz_initialize(void)
 {
-#if defined(__ARM_FEATURE_CRC32)
+#if !defined(__ARM_FEATURE_CRC32)
 	__slz_make_crc_table();
 #endif
 	__slz_prepare_dist_table();

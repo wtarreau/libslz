wtarreau/libslz

commit d8715db20b2968d1f3012a734021c0978758f911

slz: do not produce the crc32_fast table when CRC is natively supported
On ARM with native CRC support, no need to inflate the executable with
a 4kB CRC table, let's just drop it.

src/slz.c
@@ -1319,6 +1319,8 @@ void slz_prepare_dist_table()
 __attribute__((constructor))
 static void __slz_initialize(void)
 {
+#if defined(__ARM_FEATURE_CRC32)
 	__slz_make_crc_table();
+#endif
 	__slz_prepare_dist_table();
 }

src/tables.h
@@ -126,11 +126,15 @@ static const uint32_t len_fh[259] = {
 
 #ifndef PRECOMPUTE_TABLES
 
+#if !defined(__ARM_FEATURE_CRC32)
 static uint32_t crc32_fast[4][256];
+#endif
+
 static uint32_t fh_dist_table[32768];
 
 #else  /* ifndef PRECOMPUTE_TABLES */
 
+#if !defined(__ARM_FEATURE_CRC32)
 /* Table of *inverted* CRC32 for each 8-bit quantity based on the position of
  * the byte being read relative to the last byte. Eg: [0] means we're on the
  * last byte, [1] on the previous one etc. These values have 8 inverted bits
@@ -405,6 +409,7 @@ static const uint32_t crc32_fast[4][256] = {
 		0x12b5af11, 0xaa09c874, 0xb8bc679a, 0x000000ff, // 252-255
 	}
 };
+#endif // #if !defined(__ARM_FEATURE_CRC32)
 
 /* This table contains the fixed huffman encoding of all individual match
  * distances in order not to have to compute them. It was generated by
@@ -4518,7 +4523,8 @@ static const uint32_t fh_dist_table[32768] = {
  */
 static inline void __slz_make_crc_table(void)
 {
-#ifndef PRECOMPUTE_TABLES
+#if !defined(PRECOMPUTE_TABLES) && !defined(__ARM_FEATURE_CRC32)
+
 	uint32_t c;
 	int n, k;
 

commit 015a1b2cf2c53de26b6572134a41a02b79d55274

slz: fix broken initialization of CRC tables
A typo was introduced in commit d8715db ("slz: do not produce the
crc32_fast table when CRC is natively supported") causing the
condition to initialize the tables to be inverted. As a result,
machines which do *not* have a hardware CRC32 instruction do not
initialize the tables. The code which was tested on haproxy did
have it, and the tests run on ARM to validate the build didn't
show any error.

Thanks to Nicolas Boos for reporting the problem!

src/slz.c
@@ -1319,7 +1319,7 @@ void slz_prepare_dist_table()
 __attribute__((constructor))
 static void __slz_initialize(void)
 {
-#if defined(__ARM_FEATURE_CRC32)
+#if !defined(__ARM_FEATURE_CRC32)
 	__slz_make_crc_table();
 #endif
 	__slz_prepare_dist_table();