b0a976b644a8ed7212fb4141a6df02e020a157ce
Willy Tarreau <w@1wt.eu>
2026-07-27 19:20:45 +0200

    zdec: implement various command-line options
    Most of the options available in zenc for debugging and testing were
    just copied as-is to zdec in order to permit to run tests as well:
    
      -b <size>  use a <size> bytes output ring (default %d, min %d)
      -c         send output to stdout [default]
      -h         display this help
      -i <size>  read the input by chunks of <size> bytes (default %d)
      -l <loops> loop <loops> times over the same file (needs a seekable input)
      -t         test mode: decompress but do not emit anything
      -v         increase verbosity
      -D         input is raw Deflate (RFC1951), which cannot be detected
      -G         input is Gzip (RFC1952)
      -Z         input is Zlib (RFC1950)
    
    Notably, -t and -v also work together to output input and output sizes
    without producing output data, and the envelope format can now be forced.
    
    It would be nice to backport this to 1.3 if possible.
    
    (cherry picked from commit 5b9ddd6f9af7615d2bc368a3dc1573881706cfe4)

--- src/zdec.c
+++ src/zdec.c
@@ -25,73 +25,196 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "slz.h"
 
-int main(void)
-{
-	unsigned char obuf[32768];
-	unsigned char inbuf[8192];
-	struct uslz_stream state;
-	long decoded_total = 0;
+#ifndef SLZ_VERSION
+#define SLZ_VERSION "unknown"
+#endif
 
-	if (!uslz_init(&state, obuf, sizeof(obuf))) {
-		fprintf(stderr, "failed to init uslz state\n");
+/* the ring must be at least 32 kB, see uslz_init() */
+#define MIN_RING 32768
+
+static void usage(const char *name, int ret)
+{
+	printf("zdec version %s\n"
+	       "Usage: %s [option]* [file]\n"
+	       "\n"
+	       "Decompresses <file> or stdin to stdout. The stream format is\n"
+	       "detected automatically unless one of -D/-G/-Z is given.\n"
+	       "\n"
+	       "The following arguments are supported :\n"
+	       "  -b <size>  use a <size> bytes output ring (default %d, min %d)\n"
+	       "  -c         send output to stdout [default]\n"
+	       "  -h         display this help\n"
+	       "  -i <size>  read the input by chunks of <size> bytes (default %d)\n"
+	       "  -l <loops> loop <loops> times over the same file (needs a seekable input)\n"
+	       "  -t         test mode: decompress but do not emit anything\n"
+	       "  -v         increase verbosity\n"
+	       "\n"
+	       "  -D         input is raw Deflate (RFC1951), which cannot be detected\n"
+	       "  -G         input is Gzip (RFC1952)\n"
+	       "  -Z         input is Zlib (RFC1950)\n"
+	       "\n", SLZ_VERSION, name, MIN_RING, MIN_RING, 8192);
+	exit(ret);
+}
+
+int main(int argc, char **argv)
+{
+	unsigned char *obuf, *inbuf;
+	const char *file = NULL;
+	struct uslz_stream state;
+	long ring_size = MIN_RING;
+	long in_size = 8192;
+	long decoded_total;
+	int test_only = 0;
+	int verbose = 0;
+	int format = -1;       /* -1 = detect */
+	int loops = 1;
+	int fd = 0;
+	int ret;
+
+	while (argc > 1 && argv[1][0] == '-') {
+		if (strcmp(argv[1], "-b") == 0 && argc > 2) {
+			ring_size = atol(argv[2]);
+			argv++; argc--;
+		}
+		else if (strcmp(argv[1], "-i") == 0 && argc > 2) {
+			in_size = atol(argv[2]);
+			argv++; argc--;
+		}
+		else if (strcmp(argv[1], "-l") == 0 && argc > 2) {
+			loops = atoi(argv[2]);
+			argv++; argc--;
+		}
+		else if (strcmp(argv[1], "-t") == 0)
+			test_only = 1;
+		else if (strcmp(argv[1], "-c") == 0)
+			test_only = 0;
+		else if (strcmp(argv[1], "-v") == 0)
+			verbose++;
+		else if (strcmp(argv[1], "-D") == 0)
+			format = SLZ_FMT_DEFLATE;
+		else if (strcmp(argv[1], "-G") == 0)
+			format = SLZ_FMT_GZIP;
+		else if (strcmp(argv[1], "-Z") == 0)
+			format = SLZ_FMT_ZLIB;
+		else if (strcmp(argv[1], "-h") == 0)
+			usage(argv[0], 0);
+		else {
+			fprintf(stderr, "unknown option '%s'\n", argv[1]);
+			usage(argv[0], 1);
+		}
+		argv++; argc--;
+	}
+
+	if (argc > 1)
+		file = argv[1];
+
+	if (ring_size < MIN_RING || in_size < 1) {
+		fprintf(stderr, "ring size must be at least %d and input chunk at least 1\n", MIN_RING);
 		exit(1);
 	}
 
-	while (1) {
-		unsigned char *input;
-		unsigned char *decoded;
-		long decoded_len;
-		long consumed;
-		enum uslz_decode_ret decode_ret;
-		int rret, wret;
-		uint32_t crc;
+	obuf = malloc(ring_size);
+	inbuf = malloc(in_size);
+	if (!obuf || !inbuf) {
+		fprintf(stderr, "out of memory\n");
+		exit(1);
+	}
 
-		rret = read(0, inbuf, sizeof(inbuf));
-		if (rret <= 0) {
-			fprintf(stderr, "read error\n");
+	if (file) {
+		fd = open(file, O_RDONLY);
+		if (fd < 0) {
+			perror("open");
 			exit(1);
 		}
-		input = inbuf;
+	}
+
+	if (loops > 1 && lseek(fd, 0, SEEK_SET) == (off_t)-1) {
+		fprintf(stderr, "input is not seekable, cannot loop\n");
+		exit(1);
+	}
+
+	while (loops--) {
+		int done = 0;
+
+		if (lseek(fd, 0, SEEK_SET) == (off_t)-1 && loops) {
+			fprintf(stderr, "input is not seekable, cannot loop\n");
+			exit(1);
+		}
+
+		decoded_total = 0;
+
+		if (format < 0)
+			ret = uslz_init(&state, obuf, ring_size);
+		else
+			ret = uslz_init_fmt(&state, obuf, ring_size, format);
+
+		if (!ret) {
+			fprintf(stderr, "failed to init uslz state\n");
+			exit(1);
+		}
+
+		while (!done) {
+			unsigned char *input;
+			unsigned char *decoded;
+			long decoded_len;
+			long consumed;
+			enum uslz_decode_ret decode_ret;
+			long rret;
+
+			rret = read(fd, inbuf, in_size);
+			if (rret <= 0) {
+				if (rret < 0)
+					perror("read");
+				else
+					fprintf(stderr, "truncated stream\n");
+				exit(1);
+			}
+			input = inbuf;
  redo:
-		decode_ret = uslz_decode(&state, input, rret, &decoded, &decoded_len, &consumed, &crc);
-		switch(decode_ret) {
+			decode_ret = uslz_decode(&state, input, rret, &decoded,
+			                         &decoded_len, &consumed, NULL);
+			switch (decode_ret) {
 			case USLZ_DECODE_SUCCESS:
 			case USLZ_DECODE_OUT_OF_SPACE:
 			case USLZ_DECODE_OUT_OF_DATA:
-			{
-				while (decoded_len) {
-					wret = write(1, decoded, decoded_len);
+				decoded_total += decoded_len;
+
+				while (!test_only && decoded_len) {
+					long wret = write(1, decoded, decoded_len);
+
 					if (wret < 0) {
-						fprintf(stderr, "write error\n");
+						perror("write");
 						exit(1);
 					}
 					decoded += wret;
-					decoded_total += wret;
 					decoded_len -= wret;
 				}
 
-				if (decode_ret == USLZ_DECODE_SUCCESS) {
-					/* printf mainly useful for debug purposes, let's be
-					 * silent by default
-					 */
-					//fprintf(stderr, "crc = %08x, size = %lu\n", crc, decoded_total);
-					exit(0);
-				}
-				/* out of space */
 				input += consumed;
 				rret -= consumed;
-				if (decode_ret == USLZ_DECODE_OUT_OF_DATA)
-					continue;
-				goto redo;
-			}
+
+				/* A gzip stream may be a series of members, so
+				 * keep feeding while there is input left rather
+				 * than stopping on the first success.
+				 */
+				if (decode_ret == USLZ_DECODE_SUCCESS && !rret)
+					done = 1;
+				else if (decode_ret != USLZ_DECODE_OUT_OF_DATA)
+					goto redo;
+				break;
 			default:
 				fprintf(stderr, "error (%d)\n", decode_ret);
 				exit(1);
-				break;
+			}
 		}
+
+		if (verbose)
+			fprintf(stderr, "totout=%ld\n", decoded_total);
 	}
 	return 0;
 }
