a572c3f46f0138b657b245effedf23659cf1b204
Willy Tarreau <w@1wt.eu>
2026-08-29 21:20:59 +0200

    slz: use __builtin_ctzl() to finish in memmatch()
    Interestingly, it would produce bad and slow code before the cleanup and
    now it's slightly faster and involves a tzcnt instruction instead of a bsf.
    This is the only change that can be observed in the binary:
    
      before:
        3d3:       48 0f bc d2             bsf    %rdx,%rdx
    
        $ time taskset -c 4  ./zenc -4 -D -l 3 -t -v < /dev/shm/silesia.tar
        totin=635873280 totout=275348802 ratio=43.30% crc32=00000000 time=0.581s 730.1MB/s
    
      after:
        3d3:       f3 48 0f bc d2          tzcnt  %rdx,%rdx
    
        $ time taskset -c 4  ./zenc -4 -D -l 3 -t -v < /dev/shm/silesia.tar
        totin=635873280 totout=275348802 ratio=43.30% crc32=00000000 time=0.580s 731.3MB/s
    
    It looks 100% reproducible, not sure why tzcnt is faster than bsf here
    (and particularly why the construct was slower before, maybe because of
    the casts). Using this builtin will ensure that non-x86 platforms also
    run faster (e.g. arm).

--- src/slz.c
+++ src/slz.c
@@ -332,36 +332,7 @@ static inline long memmatch(const unsigned char *a, const unsigned char *b, long
 	return len;
 
  end:
-#if defined(__x86_64__) || defined(__i386__) || defined(__i486__) || defined(__i586__) || defined(__i686__)
-	/* x86 has bsf. We know that xor is non-null here */
-	asm("bsf %1,%0\n" : "=r"(xor) : "0" (xor));
-	return len + (long)(xor / 8);
-#else
-	if (sizeof(long) > 4 && !(xor & 0xffffffff)) {
-		/* This code is optimized out on 32-bit archs, but we still
-		 * need to shift in two passes to avoid a warning. It is
-		 * properly optimized out as a single shift.
-		 */
-		xor >>= 16; xor >>= 16;
-		if (xor & 0xffff) {
-			if (xor & 0xff)
-				return len + 4;
-			return len + 5;
-		}
-		if (xor & 0xffffff)
-			return len + 6;
-		return len + 7;
-	}
-
-	if (xor & 0xffff) {
-		if (xor & 0xff)
-			return len;
-		return len + 1;
-	}
-	if (xor & 0xffffff)
-		return len + 2;
-	return len + 3;
-#endif // x86
+	return len + (__builtin_ctzl((unsigned long)xor) >> 3);
 
 #else // UNALIGNED_LE_OK
 	/* This is the generic version for big endian or unaligned-incompatible
